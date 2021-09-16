f15f67a2417864253603ac747d0769e8bf495885
Sam Newby <sam.newby@mail.utoronto.ca>
2021-09-16 12:07:44 -0400

    adds basic account info

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sort_memories
+import account_info
 import sys
 
 if __name__ == '__main__':
     print('1. Sort Memories')
+    print('2. Show account details')
     print('0. Exit')
     valid_choice = False
     while not valid_choice:
@@ -10,6 +12,8 @@ if __name__ == '__main__':
         choice = input('Select option: ').strip(' ')
         if choice == '1':
             sort_memories.sortSnapchatMemories()
+        if choice == '2':
+            account_info.showAccountInfo()
         elif choice == '0':
             sys.exit('Exiting program...')
         else:
